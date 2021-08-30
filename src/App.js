c2d6339bc135d6eaeec3242bc12dca0d81ef212b
Neil M <neilm813@gmail.com>
2021-08-29 17:05:01 -0700

    initialize state and conditonal rendering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React, { useState } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleFetchClick = (_e) => {
+    setData("test");
+    // setError("test");
+  };
+
+  const fetchBtnText = data || error ? "Refresh Data" : "Fetch Data";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleFetchClick}>{fetchBtnText}</button>
+
+      {!error && <div>{data}</div>}
     </div>
   );
 }
