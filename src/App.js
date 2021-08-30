411c46386de887f04d5c7a8fd78f7ae9beae304f
Neil M <neilm813@gmail.com>
2021-08-29 23:31:36 -0700

    add styles and pct change from last

--- src/App.js
+++ src/App.js
@@ -33,12 +33,61 @@ function App() {
 
   const fetchBtnText = data || error ? "Refresh Data" : "Fetch Data";
 
+  let pctChange = null;
+
+  if (data && prevData) {
+    const usdPriceChange =
+      data.bpi.USD.rate_float - prevData.bpi.USD.rate_float;
+
+    pctChange = ((usdPriceChange / prevData.bpi.USD.rate_float) * 100).toFixed(
+      2
+    );
+  }
+
+  // If this were a larger project, I would use a CSS-in-js library.
   return (
     <div style={{ maxWidth: "85%", margin: "0 auto", paddingTop: 20 }}>
-      <button onClick={handleFetchClick}>{fetchBtnText}</button>
+      <button
+        onClick={handleFetchClick}
+        style={{
+          padding: 5,
+          fontSize: 20,
+        }}
+      >
+        {fetchBtnText}
+      </button>
 
-      <BTCData data={prevData} />
-      <BTCData data={data} />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: 10,
+          marginBottom: 10,
+        }}
+      >
+        <BTCData
+          data={prevData}
+          // Additional / override styles.
+          style={{
+            marginRight: 15,
+            backgroundColor: "white",
+          }}
+        />
+
+        {pctChange && (
+          <p
+            style={{
+              fontWeight: "bold",
+              color: pctChange < 0 ? "red" : "green",
+              marginRight: 15,
+            }}
+          >
+            {pctChange}%
+          </p>
+        )}
+
+        <BTCData data={data} />
+      </div>
       <DisplayError error={error} />
     </div>
   );
